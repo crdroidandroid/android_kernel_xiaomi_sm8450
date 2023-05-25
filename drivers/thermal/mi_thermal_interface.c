81b022e34edf0b73b426471be7ea95ec2f51757e
Jens Reidel <adrian@travitia.xyz>
2023-05-26 00:48:00 +0200

    drivers: Add missing function prototypes
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- drivers/thermal/mi_thermal_interface.c
+++ drivers/thermal/mi_thermal_interface.c
@@ -215,7 +215,7 @@ static int cpu_thermal_init(void)
 	return ret;
 }
 
-static void destory_thermal_cpu(){
+static void destory_thermal_cpu(void){
 	struct cpufreq_device *priv, *tmp;
 	list_for_each_entry_safe(priv, tmp, &cpufreq_dev_list, node) {
 		freq_qos_remove_request(priv->qos_req);
@@ -913,7 +913,7 @@ out:
 	return 0;
 }
 
-static void create_thermal_message_node()
+static void create_thermal_message_node(void)
 {
 	int ret = 0;
 	struct kernfs_node *sysfs_sd = NULL;
