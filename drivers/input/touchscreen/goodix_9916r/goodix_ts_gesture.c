36d3438756e6807df1cbf124282a2bc3d099cca2
chaitanyakm <chaitanyaarora3@gmail.com>
2024-08-24 22:59:39 +0530

    Revert "input: touchscreen: goodix_9916r: Only notify sysfs on double click events"
    This reverts commit 983fec03bed127753a24622c9fdfdd1e8274f913.

--- drivers/input/touchscreen/goodix_9916r/goodix_ts_gesture.c
+++ drivers/input/touchscreen/goodix_9916r/goodix_ts_gesture.c
@@ -307,7 +307,12 @@ static int gsx_gesture_ist(struct goodix_ts_core *cd,
 	case GOODIX_GESTURE_DOUBLE_TAP:
 		if (cd->gesture_type & GESTURE_DOUBLE_TAP) {
 			ts_info("get DOUBLE-TAP gesture");
-			notify_gesture_double_tap();
+/*N17 code for HQ-299273 by jiangyue at 2023/7/13 start*/
+			input_report_key(cd->input_dev, KEY_WAKEUP, 1);
+			input_sync(cd->input_dev);
+			input_report_key(cd->input_dev, KEY_WAKEUP, 0);
+/*N17 code for HQ-299273 by jiangyue at 2023/7/13 end*/
+			input_sync(cd->input_dev);
 		} else {
 			ts_debug("not enable DOUBLE-TAP");
 		}
